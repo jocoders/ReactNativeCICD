ca636b48bb6b9ec9963cb94cdf57bccf6bca6ea1
evgeniykireev <e.kireev@mercurydevelopment.com>
2023-04-09 16:01:03 +0700

    feat: created Text component

---
+++ src/components/Text/Text.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+export const Text = ({name, age}) => {
+  return (
+    <View style={SS.container}>
+      <Text>{name}</Text>
+      <Text>{age}</Text>
+    </View>
+  );
+};
+
+const SS = StyleSheet.create({
+  container: {
+    height: 100,
+    padding: 16,
+  }
+});
+
